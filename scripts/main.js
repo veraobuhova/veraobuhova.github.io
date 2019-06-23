162b5df929e4a8becf60c48b4e8b1f64672b2931
Ogion <mefor@tut.by>
2019-06-23 22:32:21 +0300

    Add skeleton

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+function onLoad() {
+  // initMap();
+}
+
+$(document).ready(onLoad);
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  // var uluru = {
+  //   lat: 51.508742,
+  //   lng: -0.120850
+  // };
+  var uluru = {
+    lat: -25.344,
+    lng: 131.036
+  };
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+    document.getElementById('map'), {
+      zoom: 5,
+      center: uluru
+    });
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
